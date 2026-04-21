c28e0a6982011198e25f3116f92ddf0357b30139
Lubdhak Nairith Saha Arnish <lubdhakarnish@gmail.com>
2026-04-21 12:56:57 +0600

    Create config.h

---
+++ config.h
@@ -0,0 +1,6 @@
+#define BLYNK_TEMPLATE_ID "YourTemplateID"
+#define BLYNK_TEMPLATE_NAME "SmartPlant"
+#define BLYNK_AUTH_TOKEN "YourAuthToken"
+
+#define WIFI_SSID "YourWiFi"
+#define WIFI_PASS "YourPassword"
